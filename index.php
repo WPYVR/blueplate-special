a35465d7e2fa25b1993240b1ebaac1667f7d4397
Cris Puno <crispuno@gmail.com>
2012-06-30 16:23:29 -0700

    Added Flexslider in index.php

--- index.php
+++ index.php
@@ -8,53 +8,20 @@ get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
-
-				<?php toolbox_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
-
-				<?php toolbox_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'toolbox' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-			
-			<div class="flexslider">
+<div class="flexslider">
   <ul class="slides">
     <li>
-      <img src="slide1.jpg" />
+      <img src="http://www.crispuno.ca/wp-content/uploads/2012/01/lulukfp-330x255.jpg" />
     </li>
     <li>
-      <img src="slide2.jpg" />
+      <img src="http://www.crispuno.ca/wp-content/uploads/2012/01/dandfp-160x115.jpg" />
     </li>
     <li>
-      <img src="slide3.jpg" />
+      <img src="http://www.crispuno.ca/wp-content/uploads/2012/01/silverfp-300x245.jpg" />
     </li>
   </ul>
 </div>
+
 
 			</div><!-- #content -->
 		</div><!-- #primary -->
