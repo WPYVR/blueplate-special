714d51bb76bb3fe5775429b9643ab17711fdeaf6
milesrh <miles_harrison@telus.net>
2012-06-30 15:18:57 -0700

    Google Font Javascipt support load
    Adding Google font support via javascriptload
    
    Signed-off-by: milesrh <miles_harrison@telus.net>

--- header.php
+++ header.php
@@ -20,6 +20,19 @@
 <!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Diplomata+SC::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
